9cb86fb8fcba548f00c3732c05c5c68a3135d275
Angel <angel.ortega5241@alumnos.udg.mx>
2023-10-14 14:35:07 -0600

    Configuracion modificada

--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,25 @@
-import adapter from '@sveltejs/adapter-netlify';
+const sveltePreprocess = require('svelte-preprocess')
+const netlify = require('@sveltejs/adapter-netlify')
+const pkg = require('./package.json')
 
-export default {
-	kit: {
-		// default options are shown
-		adapter: adapter({
-			// if true, will create a Netlify Edge Function rather
-			// than using standard Node-based functions
-			edge: false,
+/** @type {import('@sveltejs/kit').Config} */
+module.exports = {
+  // Consult https://github.com/sveltejs/svelte-preprocess
+  // for more information about preprocessors
+  preprocess: sveltePreprocess(),
+  kit: {
+    // By default, `npm run build` will create a standard Node app.
+    // You can create optimized builds for different platforms by
+    // specifying a different adapter
+     adapter: netlify(),
 
-			// if true, will split your app into multiple functions
-			// instead of creating a single one for the entire app.
-			// if `edge` is true, this option cannot be used
-			split: false
-		})
-	}
-};
+    // hydrate the <div id="svelte"> element in src/app.html
+    target: '#svelte',
+
+    vite: {
+      ssr: {
+        noExternal: Object.keys(pkg.dependencies || {}),
+      },
+    },
+  },
+}
